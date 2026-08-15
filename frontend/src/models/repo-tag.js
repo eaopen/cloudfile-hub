da287e9fedb79e49df587cb378996bd2c1ffa358
dev9-bb <openeadev9@gmail.com>
2026-08-15 23:00:45 +0800

    feat(tags): system-tag lock icon in used-tag bar (tags-006)

--- frontend/src/models/repo-tag.js
+++ frontend/src/models/repo-tag.js
@@ -4,6 +4,9 @@ class RepoTag {
     this.fileCount = object.files_count || 0;
     this.name = object.tag_name;
     this.color = object.tag_color;
+    // CloudFile P2-07: the repo-tags list returns is_system for admin-created
+    // system tags (CF_ENABLE_TAGS); the used-tag bar shows a lock for them.
+    this.isSystem = !!object.is_system;
   }
 }
 
